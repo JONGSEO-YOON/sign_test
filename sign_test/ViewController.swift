0394f318713af7ad6023880b19579601e746bfbb
윤종서 <yunjongseo@yunjongseoui-macbook-air.local>
2016-02-09 23:39:07 +0900

    Initial Commit

---
+++ sign_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sign_test
+//
+//  Created by 윤종서 on 2016. 2. 9..
+//  Copyright © 2016년 윤종서. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sign_testTests/sign_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  sign_testTests.swift
+//  sign_testTests
+//
+//  Created by 윤종서 on 2016. 2. 9..
+//  Copyright © 2016년 윤종서. All rights reserved.
+//
+
+import XCTest
+@testable import sign_test
+
+class sign_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
